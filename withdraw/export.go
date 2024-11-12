e82a0c0dbc72f6baba5eabc4deb57d8b95019d36
lsat12357 <lsato@uoregon.edu>
2024-11-12 15:54:13 -0800

    process members of set for export

--- withdraw/export.go
+++ withdraw/export.go
@@ -3,60 +3,82 @@ package withdraw
 import(
   "github.com/labstack/echo/v4"
   "log"
-  "io"
-  "net/http"
   "rds_alma_tools/connect"
+  "github.com/tidwall/gjson"
+  "os"
+  "net/url"
+  "encoding/json"
+  "strings"
 )
 
-// holds all data for return to user
-type Export struct{
-  brief_items []BriefItem
-}
-
-// only fields of interest for withdraw purposes
-type BriefItem struct{
-  mms_id string
-  title string
-  holding_id string
-  item_pid string
-  barcode string
-  base_status string
-  library string
-  location string
-  public_note string
-  fulfillment_note string
-  internal_note_1 string
-  internal_note_2 string
-  internal_note_3 string
-}
-
-func ExportSetHandler(c echo.Context)(string, error) {
-  // get set
-  param_ids := []string{ c.Param("id") }
-  json, err := connect.Get("sets", param_ids)
+func ExportSetHandler(c echo.Context)(error) {
+  base_url := os.Getenv("ALMA_URL")
+  full_url, err := url.JoinPath(base_url, "conf", "sets", c.Param("id"), "members")
+  params := []string{"limit=100", "apikey=" + os.Getenv("ALMA_KEY")}
+  if err != nil {}
+  //get set
+  set_data, err := connect.Get(full_url, params)
   if err != nil {}
   // get links for items
-  links, err := ExtractLinks(json)
-  if err != nil {}
-  var export Export
-  // iterate through items 
-  for _, link := range links {
-    export.processItem(link)
-  }
+  links := ExtractLinks([]byte(set_data))
 
-  // return export file using json stream option?
-  c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
-  c.Response().WriteHeader(http.StatusOK)
-  return json.NewEncoder(c.Response()).Encode(export)
+  //prepare file
+  f, err := os.CreateTemp("","withdraw_set-")
+  if err != nil {log.Println(err); return err}
+  defer f.Close()
+  defer os.Remove(f.Name())
+  _, err = f.WriteString(BriefItemHead())
+  if err != nil { log.Println(err); return err}
+
+  // iterate through items 
+  str := ""
+  for _, link := range links {
+    str = ProcessItem(link)
+    _, err := f.WriteString(str)
+    if err != nil {log.Println(err); return err}
+  }
+  filename := "export_" + c.Param("id")
+  return c.Inline(f.Name(), filename)
 }
 
-// returns list of links pulled from members using gjson?
-func extractLinks(json string)([]string, error){}
+// returns list of links pulled from members using gjson
+func ExtractLinks(json []byte)([]string){
+  arr := []string{}
+  members := gjson.GetBytes(json, "member.#.link")
+  for _, link := range members.Array(){
+    arr = append(arr, link.String())
+  }
+  return arr
+}
+
+func BriefItemHead()string{
+  properties := []string{
+    "mms_id",
+    "title",
+    "holding_id",
+    "pid",
+    "barcode",
+    "base_status",
+    "library",
+    "location",
+    "public_note",
+    "fulfillment_note",
+    "internal_note_1",
+    "internal_note_2",
+    "internal_note_3"}
+  return strings.Join(properties[:], "\t")
+}
 
 // called during iteration through links to items
 // creates BriefItems and adds them to the export
-func (e Export)processItem(url string){}
-
-// pulls fields of interest using gjson?
-func (b BriefItem)buildBriefItem(json string)error{}
+func ProcessItem(url string)string{
+  params := []string{"view=brief", "apikey=" + os.Getenv("ALMA_KEY")}
+  data, err := connect.Get(url, params)
+  if err != nil { log.Println(err); return "" }
+  var r Record
+  err = json.Unmarshal([]byte(data), &r)
+  if err != nil { log.Println(err); return ""}
+  str := r.Stringify()
+  return str
+}
 
---
+++ withdraw/record.go
@@ -0,0 +1,57 @@
+package withdraw
+
+import "fmt"
+
+type Record struct{
+  Bib_data Bib `json:"bib_data"`
+  Holding_data Holding `json:"holding_data"`
+  Item_data Item `json:"item_data"`
+}
+
+func (r Record)Stringify()string{
+  str := fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
+    r.Bib_data.Mms_id, 
+    r.Bib_data.Title,
+    r.Holding_data.Holding_id,
+    r.Item_data.Item_pid,
+    r.Item_data.Barcode,
+    r.Item_data.Base_status.Desc,
+    r.Item_data.Library.Value,
+    r.Item_data.Location.Value,
+    r.Item_data.Public_note,
+    r.Item_data.Fulfillment_note,
+    r.Item_data.Internal_note_1,
+    r.Item_data.Internal_note_2,
+    r.Item_data.Internal_note_3)
+  return str
+}
+
+type Bib struct{
+  Mms_id string `json:"mms_id"`
+  Title string `json:"title"`
+}
+
+type Holding struct{
+  Holding_id string `json:"holding_id"`
+}
+
+type Item struct{
+  Item_pid string `json:"pid"`
+  Barcode string `json:"barcode"`
+  Base_status Desc `json:"base_status"`
+  Library Value `json:"library"`
+  Location Value `json:"location"`
+  Public_note string `json:"public_note"`
+  Fulfillment_note string `json:"fulfillment_note"`
+  Internal_note_1 string `json:"internal_note_1"`
+  Internal_note_2 string `json:"internal_note_2"`
+  Internal_note_3 string `json:"internal_note_3"`
+}
+
+type Value struct{
+  Value string `json:"value"`
+
+}
+type Desc struct{
+  Desc string `json:"desc"`
+}
