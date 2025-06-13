0c28fe42fa1d03a976e5663207b2e89ee81dabdf
lsat12357 <lsato@uoregon.edu>
2025-06-13 07:42:15 -0700

    use file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
   "rds_alma_tools/withdraw"
   "github.com/labstack/echo/v4"
   "github.com/labstack/echo/v4/middleware"
+  "os"
 )
 
 func main() {
@@ -11,11 +12,13 @@ func main() {
   // Middleware
   e.Use(middleware.Logger())
   e.Use(middleware.Recover())
+  home := os.Getenv("HOME_DIR")
 
   e.GET("/withdraw/export/:id", withdraw.ExportSetHandler)
   e.POST("/withdraw/process", withdraw.ProcessHandler)
-  e.Static("/withdraw", "views/withdraw") //urlpath,directorypath, withdraw/set.html
   e.Static("/reports", "views/reports")
+  e.File("/withdraw/set.html", home + "/views/withdraw/set.html")
   e.GET("/report_list", withdraw.ListReportsHandler)
+  e.File("/version", home + "/version.txt")
   e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
